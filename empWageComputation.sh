801da05e628dc29a1b9c7de7113ce6b130b49859
“Aniket_D” <aniketdakhare@gmail.com>
2020-05-28 12:25:10 +0530

    Daily Wages Calculated

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -x
 
 echo Welcome to Employee Wage Computation Program
-
+wageperHour=20
+dailyHours=8
 present=1
 attendance=$((RANDOM%2))
 if [ $attendance -eq $present ]
 then
    echo "Employee Present"
+	dailyhours=8
 else
    echo "Emmployee Absent"
+	dailyhours=0
 fi
+perDayWage=$(($dailyHours*$wageperHour))
+
+echo "Daily Employee Wage : Rs. $perDayWage "
