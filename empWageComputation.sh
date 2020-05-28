eb4d344e9464ad24d101ce04b0eaef873d3364c2
“Aniket_D” <aniketdakhare@gmail.com>
2020-05-28 12:08:48 +0530

    Attendance Checked

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,12 @@
+#!/bin/bash -x
+
 echo Welcome to Employee Wage Computation Program
+
+present=1
+attendance=$((RANDOM%2))
+if [ $attendance -eq $present ]
+then
+   echo "Employee Present"
+else
+   echo "Emmployee Absent"
+fi
