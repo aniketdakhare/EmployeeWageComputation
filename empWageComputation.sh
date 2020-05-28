04ed0705b1ace6ae8a42985fb71420d273f9cdde
“Aniket_D” <aniketdakhare@gmail.com>
2020-05-28 12:49:56 +0530

    Calculated Fulltime and Part Time Employee Wage by case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,15 +6,20 @@ dailyHours=8
 fullTime=1
 partTime=2
 empCheck=$((RANDOM%3))
-if [ $empCheck -eq $fullTime ]
-then
-	dailyHours=8
-elif [ $empCheck -eq $partTime ]
-then
-	dailyHours=4
-else
-	dailyHours=0
-fi
+case $empCheck in
+	$fullTime)
+		echo "Fulltime Employee"
+		dailyHours=8
+		;;
+	$partTime)
+		echo "Part Time Employee"
+		dailyHours=4
+		;;
+	*)
+		echo "Employee is Absent"
+		dailyHours=0
+		;;
+esac
 empWage=$(($dailyHours*$wageperHour))
 
 echo "Daily Employee Wage : Rs. $empWage "
